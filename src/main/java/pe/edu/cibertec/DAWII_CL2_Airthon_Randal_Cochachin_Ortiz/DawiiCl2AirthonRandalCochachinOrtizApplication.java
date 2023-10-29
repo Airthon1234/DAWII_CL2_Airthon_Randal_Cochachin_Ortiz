1db694e5fb85c139166fecb630fe47a474d50e2f
Airthon1234 <cipoteland@gmail.com>
2023-10-28 19:50:10 -0500

    primera subida inicio

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_Airthon_Randal_Cochachin_Ortiz/DawiiCl2AirthonRandalCochachinOrtizApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_Airthon_Randal_Cochachin_Ortiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2AirthonRandalCochachinOrtizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2AirthonRandalCochachinOrtizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL2_Airthon_Randal_Cochachin_Ortiz/DawiiCl2AirthonRandalCochachinOrtizApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_Airthon_Randal_Cochachin_Ortiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2AirthonRandalCochachinOrtizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
